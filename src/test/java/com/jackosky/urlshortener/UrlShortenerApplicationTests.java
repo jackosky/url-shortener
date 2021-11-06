6a56f0882e65675877dbf83b4a55de6dc695509f
Jacek <jckporeda@gmail.com>
2021-11-06 13:24:11 +0100

    Initial commit

---
+++ src/test/java/com/jackosky/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackosky.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
